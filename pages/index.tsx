6a40bd471ccbac2eefb46a5d11f528cc456dc6b9
Arka <arkadatta2002@gmail.com>
2022-06-23 16:06:05 +0530

    "Frontend-Login-Page"

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>NFT DROP</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className="">Welcome to the NFT DROP WEBSITE</h1>
+
+    
+    </div>
+  )
+}
+
+export default Home
---
+++ pages/nft/[id].tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+
+const NFTDropPage = () => {
+
+  // Auth 
+  const connectWithMetamask = useMetamask()
+  const address = useAddress();
+  const disconnect = useDisconnect();
+
+  // --
+  return (
+    <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
+        {/* Left */}
+        <div className="bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4">
+            <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
+                <div className="bg-gradient-to-br from-yellow-400 to-purple-600 p-2">
+                    <img className="w-44 rounded-xl object-cover lg:h-96 lg:w-72" src="https://links.papareact.com/8sg"  />
+                </div>
+                <div className="p-5 text-center space-y-2">
+                    <h1 className="text-4xl text-white">PAPAFAM Apes</h1>
+                    <h2 className="text-xl text-gray-300">A collection of PAPAFAM Apes who live and breathe react</h2>
+                </div>
+            </div>
+        </div>
+
+        {/* Right */}
+        <div className='flex flex-1 flex-col p-12 lg:col-span-6'>
+            {/* Header */}
+
+            <header className="flex items-center justify-between">
+                <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-80">THE <span className="font-extrabold underline decoration-pink-600/50">PAPAFAM</span> NFT Market Place</h1>
+                <button onClick={() =>{ address ? disconnect() : connectWithMetamask()}} className="rounded-full text-white px-4 py-2 text-xs font-bold lg:py-3 lg:px-5 lg:text-base bg-rose-400">{ address ? 'Sign Out' : "Sign In"}</button>
+            </header>
+
+            <hr className="my-2 border"/>
+            {address && (
+                <p className="text-center text-sm text-rose-400">You're logged in with the wallet {address.substring(0,5)}...{address.substring(address.length- 5,address.length)}</p>
+            )}
+
+            {/* Content */}
+
+            <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
+                <img className="w-80 object-cover pb-10 lg:h-40" src="http://links.papareact.com/bdy" alt="" />
+                <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold"> The PAPAFAM Ape Coding Club | NFT Drop</h1>
+                <p className="pt-2 text-xl text-green-500"> 13 / 21 NFT's claimed </p>
+            </div>
+
+            {/* Mint Button */}
+            <button className="font-bold h-16 mt-10 bg-red-600 text-white w-full rounded-full">
+                Mint NFT (0.01 ETH)
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default NFTDropPage
